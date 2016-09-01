1d3812b8d095008d18514b3477679486a6cbf7b0
sethvincent <sethvincent@gmail.com>
2016-09-01 15:29:07 -0700

    .

---
+++ challenges/introduction/i18n.js
@@ -0,0 +1,10 @@
+module.exports = {
+  en: {
+    title: 'Introduction',
+    slug: 'intro'
+  },
+  es: {
+    title: 'Introducción',
+    slug: 'intro'
+  }
+}
---
+++ challenges/next-steps/i18n.js
@@ -0,0 +1,10 @@
+module.exports = {
+  en: {
+    title: 'Next steps',
+    slug: 'next'
+  },
+  es: {
+    title: 'Próximos pasos',
+    slug: 'next'
+  }
+}
